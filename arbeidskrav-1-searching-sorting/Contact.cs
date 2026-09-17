e5f583a32f4037dc57ab67489a7d02d447831bc6
Berken Ates <berken_93@hotmail.com>
2026-09-17 18:45:42 +0200

    Added Contact Class

--- arbeidskrav-1-searching-sorting/Contact.cs
+++ arbeidskrav-1-searching-sorting/Contact.cs
@@ -2,5 +2,50 @@
 
 public class Contact
 {
+    private readonly string _firstName;
+    private readonly string _lastName;
+    private readonly string _mobile;
+    private readonly string _birthday;
+    private readonly string _street;
+    private readonly string _city;
     
+    public string FirstName
+    {
+        get { return _firstName; }
+    }
+    public string LastName
+    {
+        get { return _lastName; }
+    }
+    public string Mobile
+    {
+        get { return _mobile; }
+    }
+    public string Birthday
+    {
+        get { return _birthday; }
+    }  
+    public string Street
+    {
+        get { return _street; }
+    }
+    public string City
+    {
+        get { return _city; }
+    }
+
+    public Contact (string firstName, string lastName, string mobile, string birthday, string street, string city)
+    {
+        _firstName = firstName;
+        _lastName = lastName;
+        _mobile = mobile;
+        _birthday = birthday;
+        _street = street;
+        _city = city;
+    }
+    
+    public override string ToString()
+    {
+        return $"{_firstName} {_lastName} {_mobile} {_birthday} {_street} {_city}";
+    }
 }
